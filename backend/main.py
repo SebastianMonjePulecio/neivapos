081ef390bfad3e8bf5f96742f19e3c2461c3e925
SebastianMonjePulecio <monjesebas7@gmail.com>
2026-03-24 20:33:27 -0500

    add root

--- backend/main.py
+++ backend/main.py
@@ -13,3 +13,7 @@ Base.metadata.create_all(bind=engine)
 app.include_router(products.router)
 app.include_router(sales.router)
 app.include_router(auth.router)
+
+@app.get("/")
+def home():
+    return {"mensaje": "API funcionando 🚀"}
