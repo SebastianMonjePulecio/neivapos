c05cbe2a54e4d21ae6f9d71905c58bea75d0f5e4
SebastianMonjePulecio <monjesebas7@gmail.com>
2026-03-25 11:40:28 -0500

    fix db main

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from backend.database import engine, Base
+
+# 🔥 IMPORTANTE: importar modelos
+from backend.models import user  
+
 from backend.routes import products, sales, auth
 
 app = FastAPI()
 
+# 🔥 CREA TABLAS
 Base.metadata.create_all(bind=engine)
 
 app.include_router(products.router)
